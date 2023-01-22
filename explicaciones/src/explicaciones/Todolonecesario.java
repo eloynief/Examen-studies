406440de4ed78a19093355bdb7402e4b35295f43
Usuario <Usuario@Equipo>
2023-01-22 17:14:26 +0100

    explicaciones

---
+++ explicaciones/src/explicaciones/Todolonecesario.java
@@ -0,0 +1,16 @@
+package explicaciones;
+
+public class Todolonecesario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+	public accede esta variable o método desde cualquier clase
+	protected accede esta variable o método desde otra clase que este en el mismo paquete o desde una subclase(sin importar en que paquete este)
+	private esta variable o método es privado, no se puede acceder fuera de su propia clase.
+*/
+		
+		
+	}
+
+}
